71903d47da11774bb686429a321236db2dd81b04
masfreddy93 <masieronicola@gmail.com>
2022-08-01 13:19:50 +0200

    add function generaBombe

--- js/app.js
+++ js/app.js
@@ -3,11 +3,16 @@ const colonneGriglia = 10;
 const numeroCelle = colonneGriglia ** 2;
 const bottone = document.querySelector('.bottone');
 
+let arrayBombe = [];
+
 
 bottone.addEventListener('click', function() {
 
     // resettare la griglia
     griglia.innerHTML = '';
+
+
+    arrayBombe = generaBombe(numeroCelle);
 
     for(let i=0; i<numeroCelle; i++) {
         let cella = getCellaElement();
@@ -17,6 +22,32 @@ bottone.addEventListener('click', function() {
     }
 
 }); 
+
+
+//generare array di 16 numeri che saranno le nostre bombe
+//costruire funzione
+function generaBombe(max){
+    const bombe = [];
+
+    while(bombe.length < 16){
+        const n = getRandomIntInclusive(1,max);
+
+        //se il numero non è presente nell'array bome
+        if(!bombe.includes(n)){
+            bombe.push(n);
+        }
+    }
+    console.log(bombe);
+    return bombe;
+    
+}
+
+//funzione random presa da mdn
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
 
 
 function getColourLightBlue(clickEvent) {
